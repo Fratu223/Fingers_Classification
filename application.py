a17f2b2937d0384b3a20b09ba1d92da3597a875b
Fratu223 <mihaifratu78@gmail.com>
2024-08-07 15:04:25 +0200

    Changes

--- application.py
+++ application.py
@@ -12,26 +12,19 @@ while True:
     if not ret:
         break
     
-    # Flip the frame horizontally
     frame = cv2.flip(frame, 1)
 
-    # Preprocess the frame
     img = preprocess_image(frame)
 
-    # Predict using the model
     predictions = model.predict(img)
     predicted_class = np.argmax(predictions)
 
-    # Display the predicted number of fingers
     cv2.putText(frame, f'Fingers: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-    # Show the frame
     cv2.imshow('Finger Count', frame)
 
-    # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the webcam and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
